48c882dd4eee2cf43f516caa17ea32e5b6567b53
Priviledge <privilledgeblack8@gmail.com>
2024-08-16 21:34:18 +0200

    added the homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Events from './components/Events';
+import Sports from './components/Sports';
+import ClubPic from './components/ClubPic';
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
    <Header/>
    <Routes>
      <Route index element={<Home/>}/>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/Events' element={<Events/>}/>
+      <Route path='/Sports' element={<Sports/>}/>
+      <Route path='/ClubPic' element={<ClubPic/>}/>
    </Routes>
    </Router>
    </>
