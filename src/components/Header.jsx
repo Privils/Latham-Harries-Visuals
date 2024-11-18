011b3540c6fe3d677a72fb87c12df916c5cd75ef
Priviledge <privilledgeblack8@gmail.com>
2024-11-18 18:59:02 +0200

    added new styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
   const restrictedPaths = ['/events', '/clubPics', '/sports'];
 
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <nav className="navbar navbar-expand-lg" >
+    <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <p className="navbar-brand logo text-white">lathamharris visuals</p>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <p className="navbar-brand">lathamharris visuals</p>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+        <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarNavDropdown">
           <ul className="navbar-nav">
             {location.pathname !== '/' && (
               <li className="nav-item">
-                <Link className="nav-link active btn-danger" to="/">
+                <Link className="nav-link active" to="/">
                   Home
                 </Link>
               </li>
@@ -38,21 +35,21 @@ const Header = () => {
                   <a className="nav-link" href="#contact">Contact</a>
                 </li>
                 <li className="nav-item dropdown">
-                  <Link
+                  <a
                     className="nav-link dropdown-toggle"
-                    to="#"
+                    href="#"
                     role="button"
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
                   >
                     Services
-                  </Link>
-                  <ul className="dropdown-menu bg-black">
+                  </a>
+                  <ul className="dropdown-menu">
                     <li>
                       <Link className="dropdown-item" to="/sports">Sports</Link>
                     </li>
                     <li>
-                      <Link className="dropdown-item" to="/events">Events</Link> 
+                      <Link className="dropdown-item" to="/events">Events</Link>
                     </li>
                     <li>
                       <Link className="dropdown-item" to="/clubPics">Club</Link>
